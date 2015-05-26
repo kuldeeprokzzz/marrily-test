c1a35b87c01f7b7490e36c76c34bf8af1fb7f6b1
KULDEEP MALIK <kuldeep.malik@mantralabsglobal.com>
2015-05-26 12:18:05 +0100

    first commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,55 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    
+  add : function (req, res) {
+        var names = ['parag','kuldeep','neha','harsh','ankita','priyanka','mikhail','arun','rose','nikita' ];
+        var genders = ['male', 'female'];
+        var professions = ['doctor','engineer','pilot','sailor','painter','sportsman','teacher','joker','watchman','principal'];
+        var ageGroups = ['20-30', '30-40', '40-50','50-70'];
+       
+        var users = [];
+       
+       for (var i = 0; i < 500000; i++ ) {
+            var data = {};
+            data.names = names[Math.floor(Math.random() * names.length)]; 
+            data.genders = genders[Math.floor(Math.random() * genders.length)];
+            data.professions = professions[Math.floor(Math.random() * professions.length)];
+            data.ageGroups = ageGroups[Math.floor(Math.random() * ageGroups.length)];
+            users.push(data);
+       }
+
+       var j = 0;
+       var cb = function(err, user){
+        j++;
+        User.addingData(users[j], cb);
+       }
+       
+       User.addingData(users[j], cb);
+       res.json({msg: 'ok'});
+    },  
+
+ 
+  pickup : function (req,res) {
+       User.pickup(req.body,function (err, data) {
+            if (err) {
+                res.forbidden();
+            } else {
+                res.json(data);
+                
+            }
+        });
+    
+       
+    }      
+    
+  
+
+    
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,74 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+       names: {
+       	type :"string",
+       	required: true
+          },
+
+       genders : {
+       	type : "string",
+       	required: true
+       },   
+
+       professions: {
+       	type :"string",
+       	required: true
+       },
+
+       ageGroups: {
+       	 type : "string",
+       	 required: true
+       }
+
+  },
+
+addingData: function (data, callback) {
+            sails.log.debug(data);
+            User.create(data).exec(function (err, Userdata) {
+  
+                  if (!err) {        
+                      return callback(null, Userdata)             
+                  } else {
+                      return callback(err);
+                  }
+            });
+      },
+
+
+  pickup: function(data, cb){
+    User.find().limit(1000).exec(function(err, allreadyMatchedUsers){
+      if(err){
+        cb(err,null);
+      }else{
+        allreadyMatchedUsers = _.map(allreadyMatchedUsers, function(user){
+          return user.id;
+        });
+         
+
+        User.find({id: {'!': allreadyMatchedUsers}, genders: 'female', ageGroups: '20-30' , professions : 'doctor'}).limit(10).exec(function(err, matches){
+            cb(null, matches);
+        });
+          
+      }
+
+        
+    });
+  }
+
+
+ 
+
+     
+
+      
+};
+
+
